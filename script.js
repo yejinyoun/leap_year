dc7a2695063b1922d3c1de8ae92ba555cb0130bd
yejinyoun <yejinyoun@icloud.com>
2023-03-04 22:16:27 +0100

    leap year diagram

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 "use strict";
 
 console.log("fucku");
+
+// leap year
+// 1. check if it's divisible by 4
+// - false : not a leap year
+// - true : step 2
+
+// 2. check if it's divisible by 100
+// - false : yes leap year
+// - true : step 3
+
+// 3. check if it's divisible by 400
+// - true : yes leap year
+// - false : not a leap year
